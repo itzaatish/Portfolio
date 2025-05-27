5b5d99f52a1f164a5e7cf4c1a67a951c5b5cd6b4
itzAatish073 <itzaatishkumar890@gmail.com>
2025-05-27 18:07:44 +0530

    commiting with toggling button

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom';
 // import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
